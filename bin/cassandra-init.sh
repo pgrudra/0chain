8094e832c687221583c795ad273d7ff0f883aebf
service-0chain <service@0chain.net>
2022-12-21 19:57:49 +0100

    updated docker based deployment

--- bin/cassandra-init.sh
+++ bin/cassandra-init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+chmod +x /0chain/bin/wait-for-service.sh
 /0chain/bin/wait-for-service.sh -t 0 cassandra:9042 -- echo "cassandra started"
 cqlsh -f /0chain/sql/zerochain_keyspace.sql cassandra
 cqlsh -f /0chain/sql/magic_block_map.sql cassandra
--- bin/postgres-entrypoint.sh
+++ bin/postgres-entrypoint.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-
 set -e
-
 psql=( psql --username "$POSTGRES_USER" --port "$POSTGRES_PORT" --host "$POSTGRES_HOST" )
-
 until pg_isready -h $POSTGRES_HOST
 do
 	echo "Sleep 1s and try again..."
 	sleep 1
 done
-
 export PGPASSWORD="$POSTGRES_PASSWORD"
-
 for f in /zchain/sql/*; do
 	case "$f" in
 		*.sh)     echo "$0: running $f"; . "$f" ;;
